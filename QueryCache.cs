0cb1cab73ba9012d6aa2536390f21583ca110647
Volodin, Andrey <AVolodin@rencap.ru>
2025-05-20 16:29:44 +0300

    Extend QueryCache.
    Changes to be committed:
    modified:   QueryCache.cs

--- QueryCache.cs
+++ QueryCache.cs
@@ -25,6 +25,19 @@ namespace RC.DBA
             return query0;
         }
 
+        public static SqlQueryWithCount<T> CreateOrGet<T>(DbContext ctx, string key, Func<IQueryBuilder, SqlQueryWithCount<T>> factory)
+        {
+            if (_cache.TryGetValue(key, out var query))
+                return ((SqlQueryWithCount<T>)query).GetQuery();
+
+            var queryBuilder = ctx.GetQueryBuilder();
+            var query0 = factory(queryBuilder);
+
+            _cache.TryAdd(key, query0);
+
+            return query0;
+        }
+
         public void Clear() => _cache.Clear();
 
         public bool Remove(string key, out SqlQuery query) => _cache.TryRemove(key, out query);
